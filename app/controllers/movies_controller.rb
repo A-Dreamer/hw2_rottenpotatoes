ae374f2d0474b7b76d00257aec8625c7307cb39e
A-Dreamer <sean.simpson@ntlworld.com>
2013-02-01 16:38:39 -0800

    hw2_part21

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,14 +7,26 @@ class MoviesController < ApplicationController
   end
 
   def index
-	@sort = params[:order]
-#   		@movies = Movie.all( params )
-	if @sort
-    		@movies = Movie.all( :order => @sort )
- 	else
-		@movies = Movie.all
+	sel_ratings = params[ :ratings ]
+	search_ratings = Array.new
+        @all_ratings = Hash.new
+	Movie.find_by_sql("select DISTINCT rating from movies order by rating" ).each do |rate| 
+		if sel_ratings == nil or sel_ratings.include?( rate.rating ) 
+			@all_ratings[ rate.rating ] = 1 
+			search_ratings << rate.rating
+		else
+			@all_ratings[ rate.rating ] = nil
+		end
 	end
-	instance_variable_set("@#{@sort}_header", "hilite")
+	sort_by = params[:order]
+	if sort_by
+#    		@movies = Movie.all( :order => sort_by )
+    		@movies = Movie.where( "movies.rating IN (?)", search_ratings ).order( sort_by )
+ 	else
+#		@movies = Movie.all
+		@movies = Movie.where( "movies.rating IN (?)", search_ratings )
+	end
+	instance_variable_set("@#{sort_by}_header", "hilite")
   end
 
   def new
